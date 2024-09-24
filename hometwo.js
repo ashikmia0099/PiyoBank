6d003a363f7cc7bec0e4b4e634432e2726902a6f
Ashik Mia <ashikmia5686@gmail.com>
2024-09-24 16:18:58 +0600

    second commit

--- hometwo.js
+++ hometwo.js
@@ -1,1 +1,47 @@
 
+
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+
+
+    
+    const AddMoney  = document.getElementById('input-add-money').value;
+    const AccountPin  = document.getElementById('input-add-pin').value;
+
+
+    if(AccountPin == '1234'){
+
+            
+        const AvailableBlance = document.getElementById('available-blance').innerText;
+
+        
+
+
+        const AddMoneyNumber = parseFloat(AddMoney)
+        const AvailableBlanceNumber = parseFloat(AvailableBlance)
+
+
+        const NewBalance = AddMoneyNumber + AvailableBlanceNumber
+
+        console.log(NewBalance)
+
+
+        // set new balacne 
+
+        document.getElementById('available-blance').innerText = NewBalance
+
+
+
+    }
+
+    else{
+        console.log('Account Pin is not vaild');
+    }
+
+    
+
+
+
+})
+
+
